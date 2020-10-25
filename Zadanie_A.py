d784109b2891a02c454359a5aacc3e3de76996c5
DomBoth <73012519+DomBoth@users.noreply.github.com>
2020-10-25 10:28:25 +0100

    Create Zadanie_A.py

---
+++ Zadanie_A.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Przedmiot: Informatyka
+# Kierunek studiów: Inżynierią Transportu
+# Semestr: zimowy
+# Rok akademicki: 2020/2021
+# Data (dzień.miesiąc.rok): 31.11.2020
+# Imię: Dominik
+# Nazwisko: Both
+# Numer albumu ZUT: 50408
+
+"""
+W tym miejscu przepisać temat.
+"""
